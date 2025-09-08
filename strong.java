8bca21c6e86d61984d7705aaeffb74900a8cc2eb
hari23666 <kishoreh320@gmail.com>
2025-09-08 16:05:01 +0530

    java program via files
    java program

--- strong.java
+++ strong.java
@@ -1,42 +1,37 @@
-public class strong
-{
+
+public class strong{
+
     // Function to calculate factorial of a number
-    public static int factorial(int n)
-    {
-        if (n == 0 || n == 1)
-        {
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
             return 1;
         }
         int fact = 1;
-        for (int i = 2; i <= n; i++)
-        {
+        for (int i = 2; i <= n; i++) {
             fact *= i;
         }
         return fact;
     }
 
     // Function to check if a number is strong
-    public static boolean isStrong(int num)
-    {
-        int originalNum = num;
+    public static boolean isStrongNumber(int number) {
+        int originalNumber = number;
         int sumOfFactorials = 0;
-        while (num > 0)
-        {
-            int digit = num % 10;
+        int temp = number;
+
+        while (temp > 0) {
+            int digit = temp % 10;
             sumOfFactorials += factorial(digit);
-            num /= 10;
+            temp /= 10;
         }
-        return sumOfFactorials == originalNum;
+        return sumOfFactorials == originalNumber;
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Strong numbers from 1 to 5000 are:");
-        for (int i = 1; i <= 5000; i++)
-        {
-            if (isStrong(i))
-            {
-                System.out.print(i + " ");
+        for (int i = 1; i <= 5000; i++) {
+            if (isStrongNumber(i)) {
+                System.out.println(i);
             }
         }
     }
